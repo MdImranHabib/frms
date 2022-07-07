e71096a41c8e13d189f18f715d4110b55d170a8b
MD IMRAN HABIB <mdimranhabib5@gmail.com>
2022-07-07 11:44:47 +0600

    changed the create update flat method

--- lib/services/flat.services.dart
+++ lib/services/flat.services.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:frms/models/flat.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,13 +18,20 @@ class FlatService {
   }
 
   // Create new flat
-  static Future createFlat(Flat flat) async {
-    return await http.post(Uri.parse('${url}flats'), body: flat);
+  static Future<bool> createFlat(Flat flat) async {
+    var flatMap = flat.toMap();
+    var flatBody = json.encode(flatMap);
+    var response = await http.post(Uri.parse('${url}flats'), body: flatBody);
+    return Future.value(response.statusCode == 201 ? true : false);
   }
 
   // Update a flat
   static Future updateFlat(Flat flat) async {
-    return await http.put(Uri.parse('${url}flats/${flat.id}'), body: flat);
+    var flatMap = flat.toMap();
+    var flatBody = json.encode(flatMap);
+    var response =
+        await http.put(Uri.parse('${url}flats/${flat.id}'), body: flatBody);
+    return Future.value(response.statusCode == 200 ? true : false);
   }
 
   //Delete a flat
